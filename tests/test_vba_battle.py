eb31936aa758a9a8534a3fd53a4987c0d5d9ca0b
Bryan Bishop <kanzure@gmail.com>
2013-11-09 15:53:17 -0600

    simplify the battle tests

--- tests/test_vba_battle.py
+++ tests/test_vba_battle.py
@@ -39,13 +39,16 @@ class BattleTests(unittest.TestCase):
     def setUp(self):
         # reset to whatever the bootstrapper created
         self.vba.state = self.bootstrap_state
+        self.battle = Battle(emulator=self.cry)
 
-    def test_battle_is_player_turn(self):
-        self.cry.vba.state = self.bootstrap_state
+    def test_is_in_battle(self):
+        self.assertTrue(self.battle.is_in_battle())
 
-        battle = Battle(emulator=self.cry)
+    def test_is_player_turn(self):
+        self.battle.skip_start_text()
 
-        self.assertTrue(battle.is_player_turn())
+        # the initial state should be the player's turn
+        self.assertTrue(self.battle.is_player_turn())
 
 if __name__ == "__main__":
     unittest.main()
