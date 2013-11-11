fd10868994edf46180a95329f2eaa48df9ccfd7a
Bryan Bishop <kanzure@gmail.com>
2013-11-11 12:39:50 -0600

    version bump to: v1.6.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requires = [
 
 setup(
     name="pokemontools",
-    version="1.5.0",
+    version="1.6.0",
     description="Tools for compiling and disassembling Pokémon Red and Pokémon Crystal.",
     long_description=open("README.md", "r").read(),
     license="BSD",
